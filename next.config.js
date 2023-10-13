e8d7e665b28b17f2f2e2e7f5819c65459a66dd13
William Lee <43682783+wlee221@users.noreply.github.com>
2023-10-13 11:27:27 -0700

    Accept image/avif

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    formats: ['image/webp'],
+    formats: ['image/avif'],
     remotePatterns: [
       {
         hostname: "**.unsplash.com"
